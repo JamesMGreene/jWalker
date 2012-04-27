1bf575a24b3f815a95d5f437cbbd7ec6a68e1436
James M. Greene <james.m.greene@gmail.com>
2012-04-27 17:56:50 -0500

    Replaced chai.js assertion library with expect.js due to some browser support issues from their fundamental technique of modifying the Object prototype chain, as well as some disappointment about grammar shortcomings. Also added some missing README/History/License/etc. files for Mocha.

--- test/test.jWalker.js
+++ test/test.jWalker.js
@@ -1,50 +1,50 @@
 describe('jWalker', function() {
 	/* Basic structure tests */
 	it('should be defined', function() {
-		(typeof(jWalker)).should.equal('object');
+		expect(jWalker).to.be.an('object');
 	});
 	it('should have a NodeType enumeration property defined', function() {
-		(typeof(jWalker.NodeType)).should.equal('object');
+		expect(jWalker.NodeType).to.be.an('object');
 	});
 	it('should have a NodeTypeFilter enumeration property defined', function() {
-		(typeof(jWalker.NodeTypeFilter)).should.equal('object');
+		expect(jWalker.NodeTypeFilter).to.be.an('object');
 	});
 	it('should have a NodeFilter enumeration property defined', function() {
-		(typeof(jWalker.NodeFilter)).should.equal('object');
+		expect(jWalker.NodeFilter).to.be.an('object');
 	});
 
 	describe("#isTreeWalkerSupportedNatively", function() {
 		it('should be defined', function() {
-			(typeof(jWalker.isTreeWalkerSupportedNatively)).should.equal('function');
+			expect(jWalker.isTreeWalkerSupportedNatively).to.be.a('function');
 		});
-		it('should return true when the DOM Traversal 2.0 feature is implemented', function() {
+		it('should [typically] return true when the DOM Traversal 2.0 feature is implemented', function() {
 			// Not strictly true but it SHOULD be true if browsers are not misbehaving
-			jWalker.isTreeWalkerSupportedNatively().should.equal(document.implementation.hasFeature("Traversal", "2.0"));
+			expect(jWalker.isTreeWalkerSupportedNatively()).to.equal(document.implementation.hasFeature("Traversal", "2.0"));
 		});
 		it('should return true when both the Document#createTreeWalker function and NodeFilter object are available', function() {
-			jWalker.isTreeWalkerSupportedNatively().should.equal(document.createTreeWalker && typeof(NodeFilter) !== "undefined");
+			expect(jWalker.isTreeWalkerSupportedNatively()).to.equal(document.createTreeWalker && typeof(NodeFilter) !== "undefined");
 		});
 		it('can be overridden for testing purposes', function() {
-			var originalFuncImpl= jWalker.isTreeWalkerSupportedNatively;
+			var originalFuncImpl = jWalker.isTreeWalkerSupportedNatively;
 
 			jWalker.isTreeWalkerSupportedNatively = function() { return false; };
-			jWalker.isTreeWalkerSupportedNatively().should.equal(false);
+			expect(jWalker.isTreeWalkerSupportedNatively()).to.equal(false);
 
 			jWalker.isTreeWalkerSupportedNatively = function() { return true; };
-			jWalker.isTreeWalkerSupportedNatively().should.equal(true);
+			expect(jWalker.isTreeWalkerSupportedNatively()).to.equal(true);
 
 			jWalker.isTreeWalkerSupportedNatively = function() { return "X"; };
-			jWalker.isTreeWalkerSupportedNatively().should.equal("X");
+			expect(jWalker.isTreeWalkerSupportedNatively()).to.equal("X");
 
 			jWalker.isTreeWalkerSupportedNatively = originalFuncImpl;
-			jWalker.isTreeWalkerSupportedNatively().should.not.equal("X");
-			jWalker.isTreeWalkerSupportedNatively().should.equal(originalFuncImpl());
+			expect(jWalker.isTreeWalkerSupportedNatively()).to.not.equal("X");
+			expect(jWalker.isTreeWalkerSupportedNatively()).to.equal(originalFuncImpl());
 		});
 	});
 	
 	describe('TreeWalker class', function() {
 		it('should be defined', function() {
-			(typeof(jWalker.TreeWalker)).should.equal('function');
+			expect(jWalker.TreeWalker).to.be.a('function');
 		});
 		// TODO: More tests
 		it('should have more tests', function() {
@@ -54,7 +54,7 @@ describe('jWalker', function() {
 
 	describe("#createMissingNativeApi", function() {
 		it('should be defined', function() {
-			(typeof(jWalker.createMissingNativeApi)).should.equal('function');
+			expect(jWalker.createMissingNativeApi).to.be.a('function');
 		});
 		// TODO: More tests
 		it('should have more tests', function() {
